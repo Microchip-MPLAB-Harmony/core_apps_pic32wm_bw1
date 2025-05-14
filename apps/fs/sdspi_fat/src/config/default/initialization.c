1c91f6f9df6dc70bdbc4e0b9a875a8cf4fc4c4d3
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2025-05-14 15:26:27 +0530

    [APPS] Created fs_sdspi_fat_pic32wm_bw1_curiosity demo
    MH3-114049

---
+++ apps/fs/sdspi_fat/src/config/default/initialization.c
@@ -0,0 +1,415 @@
+/*******************************************************************************
+  System Initialization File
+
+  File Name:
+    initialization.c
+
+  Summary:
+    This file contains source code necessary to initialize the system.
+
+  Description:
+    This file contains source code necessary to initialize the system.  It
+    implements the "SYS_Initialize" function, defines the configuration bits,
+    and allocates any necessary global system resources,
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include "configuration.h"
+#include "definitions.h"
+#include "device.h"
+
+
+// ****************************************************************************
+// ****************************************************************************
+// Section: Configuration Bits
+// ****************************************************************************
+// ****************************************************************************
+
+/*** FUSERID ***/
+#pragma config USER_ID =      0xffffU
+
+/*** DEVCFG0 ***/
+#pragma config TDOEN =      ON
+#pragma config SWOEN =      ON
+#pragma config TROEN =      OFF
+#pragma config ADCPOVR =      HW
+#pragma config ACCMP1_ALTEN =      PA1
+#pragma config CPENFILT =      OFF
+#pragma config RTCIN0_ALTEN =      PA3
+#pragma config RTCOUT_ALTEN =      PA4
+#pragma config VBCMODE =      DIRECT
+#pragma config SMBUSEN0 =      OFF
+#pragma config SMBUSEN1 =      OFF
+#pragma config SMBUSEN2 =      OFF
+#pragma config HPLUGDIS =      OFF
+#pragma config SLRTEN0 =      OFF
+#pragma config SLRTEN1 =      OFF
+#pragma config SLRTEN2 =      OFF
+#pragma config PCM =      SFR
+#pragma config INT0E =      OFF
+#pragma config INT0P =      NEG
+#pragma config FECCCON =         OFF
+#pragma config FRECCDIS =      OFF
+
+
+
+/*** DEVCFG1 ***/
+#pragma config ICESEL =      PGC1_PGD1
+#pragma config TRCEN =      ON
+#pragma config ZBTWKSYS =      OFF
+#pragma config CMP0_OE =      OFF
+#pragma config CMP1_OE =      OFF
+#pragma config CLASSBDIS =      OFF
+#pragma config SLRCTRL0 =      GPIO
+#pragma config SLRCTRL1 =      GPIO
+#pragma config SLRCTRL2 =      GPIO
+#pragma config SMCLR =      NO_POR
+#pragma config QSCHE_EN =      OFF
+#pragma config QSPI_HSEN =      PPS
+#pragma config SCOM0_HSEN =      PPS
+#pragma config SCOM1_HSEN =      PPS
+#pragma config SCOM2_HSEN =      PPS
+#pragma config CCL_OE =      ON
+#pragma config I2CDSEL0 =      OFF
+#pragma config I2CDSEL1 =      OFF
+#pragma config I2CDSEL2 =      OFF
+#pragma config WDTPSS =      PSS1048576
+#pragma config QSPIDDRM =      OFF
+#pragma config CLKZBREF =      OFF
+
+/*** DEVCFG2 ***/
+#pragma config ACMP_CYCLE =      _32US
+#pragma config DMTINTV =      WIN_127_128
+#pragma config PMUTEST_VDD_EN =      VDD_DIV_2
+#pragma config POSCMOD =      HS
+#pragma config WDTRMCS =      LPRC
+#pragma config SOSCSEL =      XTAL
+#pragma config WAKE2SPD =      ON
+#pragma config CKSWEN =      ON
+#pragma config FSCMEN =      ON
+#pragma config WDTPSR =      PS1048576
+#pragma config WDTSPGM =      STOPS
+#pragma config WINDIS =      NORMAL
+#pragma config WDTEN =      OFF
+#pragma config WDTWINSZ =    WINSZ_25
+#pragma config DMTCNT =      DMT31
+#pragma config DMTEN =      OFF
+
+
+/*** DEVCFG4 ***/
+#pragma config SOSCCFG =    0x0U
+#pragma config RTCEVENT_SEL =      ONE_SEC
+#pragma config RTCEVENT_EN =      OFF
+#pragma config VBKP_1KCSEL =      _32K
+#pragma config VBKP_32KCSEL =      FRC
+#pragma config VBKP_DIVSEL =      DIV_31_25
+#pragma config LPCLK_MOD =      DIV_1
+#pragma config RTCEVTYPE =      OUT
+#pragma config CPEN_DLY =      _8_LPRC
+#pragma config DSZPBOREN =      OFF
+#pragma config DSWDTPS =      DSPS5
+#pragma config DSWDTOSC =    LPRC
+#pragma config DSWDTEN =      OFF
+#pragma config DSEN =    OFF
+#pragma config UVREGROVR =      CONTROLLED
+#pragma config RTCNTM_CSEL =      RAW
+
+/*** FBCFG0 ***/
+#pragma config BINFOVALID =      VALID
+#pragma config PCSCMODE =      DUAL
+
+/*** FCPN0 ***/
+#pragma config CP =      DISABLED
+
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Driver Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+/* Following MISRA-C rules are deviated in the below code block */
+/* MISRA C-2012 Rule 7.2 - Deviation record ID - H3_MISRAC_2012_R_7_2_DR_1 */
+/* MISRA C-2012 Rule 11.1 - Deviation record ID - H3_MISRAC_2012_R_11_1_DR_1 */
+/* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+/* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+// <editor-fold defaultstate="collapsed" desc="DRV_SDSPI Instance 0 Initialization Data">
+
+/* SDSPI Client Objects Pool */
+static DRV_SDSPI_CLIENT_OBJ drvSDSPI0ClientObjPool[DRV_SDSPI_CLIENTS_NUMBER_IDX0];
+
+/* SDSPI Transfer Objects Pool */
+static DRV_SDSPI_BUFFER_OBJ drvSDSPI0TransferObjPool[DRV_SDSPI_QUEUE_SIZE_IDX0];
+
+/* SPI PLIB Interface Initialization for SDSPI Driver */
+static const DRV_SDSPI_PLIB_INTERFACE drvSDSPI0PlibAPI = {
+
+    /* SPI PLIB WriteRead function */
+    .writeRead = (DRV_SDSPI_PLIB_WRITEREAD)SERCOM2_SPI_WriteRead,
+
+    /* SPI PLIB Write function */
+    .write_t = (DRV_SDSPI_PLIB_WRITE)SERCOM2_SPI_Write,
+
+    /* SPI PLIB Read function */
+    .read_t = (DRV_SDSPI_PLIB_READ)SERCOM2_SPI_Read,
+
+    /* SPI PLIB Transfer Status function */
+    .isTransmitterBusy = (DRV_SPI_PLIB_TRANSMITTER_IS_BUSY)SERCOM2_SPI_IsTransmitterBusy,
+
+    .transferSetup = (DRV_SDSPI_PLIB_SETUP)SERCOM2_SPI_TransferSetup,
+
+    /* SPI PLIB Callback Register */
+    .callbackRegister = (DRV_SDSPI_PLIB_CALLBACK_REGISTER)SERCOM2_SPI_CallbackRegister,
+};
+
+static const uint32_t drvSDSPI0remapDataBits[]= { 0x0, 0x1, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU };
+static const uint32_t drvSDSPI0remapClockPolarity[] = { 0x0, 0x20000000 };
+static const uint32_t drvSDSPI0remapClockPhase[] = { 0x10000000, 0x0 };
+
+/* SDSPI Driver Initialization Data */
+static const DRV_SDSPI_INIT drvSDSPI0InitData =
+{
+    /* SD Card SPI PLIB API interface*/
+    .spiPlib                = &drvSDSPI0PlibAPI,
+
+    .remapDataBits          = drvSDSPI0remapDataBits,
+
+    .remapClockPolarity     = drvSDSPI0remapClockPolarity,
+
+    .remapClockPhase        = drvSDSPI0remapClockPhase,
+
+
+    /* SDSPI Number of clients */
+    .numClients             = DRV_SDSPI_CLIENTS_NUMBER_IDX0,
+
+    /* SDSPI Client Objects Pool */
+    .clientObjPool          = (uintptr_t)&drvSDSPI0ClientObjPool[0],
+
+    /* SDSPI Transfer Objects Pool */
+    .bufferObjPool          = (uintptr_t)&drvSDSPI0TransferObjPool[0],
+
+    /* SDSPI Transfer Objects Queue Size */
+    .bufferObjPoolSize      = DRV_SDSPI_QUEUE_SIZE_IDX0,
+
+    .chipSelectPin          = DRV_SDSPI_CHIP_SELECT_PIN_IDX0,
+
+    .sdcardSpeedHz          = DRV_SDSPI_SPEED_HZ_IDX0,
+
+    .pollingIntervalMs      = DRV_SDSPI_POLLING_INTERVAL_MS_IDX0,
+
+    .writeProtectPin        = SYS_PORT_PIN_NONE,
+
+    .isFsEnabled            = true,
+
+};
+// </editor-fold>
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Data
+// *****************************************************************************
+// *****************************************************************************
+/* Structure to hold the object handles for the modules in the system. */
+SYSTEM_OBJECTS sysObj;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Library/Stack Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+// <editor-fold defaultstate="collapsed" desc="File System Initialization Data">
+
+
+const SYS_FS_MEDIA_MOUNT_DATA sysfsMountTable[SYS_FS_VOLUME_NUMBER] =
+{
+    {NULL}
+};
+
+static const SYS_FS_FUNCTIONS FatFsFunctions =
+{
+    .mount             = FATFS_mount,
+    .unmount           = FATFS_unmount,
+    .open              = FATFS_open,
+    .read_t              = FATFS_read,
+    .close             = FATFS_close,
+    .seek              = FATFS_lseek,
+    .fstat             = FATFS_stat,
+    .getlabel          = FATFS_getlabel,
+    .currWD            = FATFS_getcwd,
+    .getstrn           = FATFS_gets,
+    .openDir           = FATFS_opendir,
+    .readDir           = FATFS_readdir,
+    .closeDir          = FATFS_closedir,
+    .chdir             = FATFS_chdir,
+    .chdrive           = FATFS_chdrive,
+    .write_t             = FATFS_write,
+    .tell              = FATFS_tell,
+    .eof               = FATFS_eof,
+    .size              = FATFS_size,
+    .mkdir             = FATFS_mkdir,
+    .remove_t            = FATFS_unlink,
+    .setlabel          = FATFS_setlabel,
+    .truncate          = FATFS_truncate,
+    .chmode            = FATFS_chmod,
+    .chtime            = FATFS_utime,
+    .rename_t            = FATFS_rename,
+    .sync              = FATFS_sync,
+    .putchr            = FATFS_putc,
+    .putstrn           = FATFS_puts,
+    .formattedprint    = FATFS_printf,
+    .testerror         = FATFS_error,
+    .formatDisk        = (FORMAT_DISK)FATFS_mkfs,
+    .partitionDisk     = FATFS_fdisk,
+    .getCluster        = FATFS_getclusters
+};
+
+
+
+
+static const SYS_FS_REGISTRATION_TABLE sysFSInit [ SYS_FS_MAX_FILE_SYSTEM_TYPE ] =
+{
+    {
+        .nativeFileSystemType = FAT,
+        .nativeFileSystemFunctions = &FatFsFunctions
+    }
+};
+// </editor-fold>
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Initialization
+// *****************************************************************************
+// *****************************************************************************
+// <editor-fold defaultstate="collapsed" desc="SYS_TIME Initialization Data">
+
+static const SYS_TIME_PLIB_INTERFACE sysTimePlibAPI = {
+    .timerCallbackSet = (SYS_TIME_PLIB_CALLBACK_REGISTER)TC0_TimerCallbackRegister,
+    .timerStart = (SYS_TIME_PLIB_START)TC0_TimerStart,
+    .timerStop = (SYS_TIME_PLIB_STOP)TC0_TimerStop,
+    .timerFrequencyGet = (SYS_TIME_PLIB_FREQUENCY_GET)TC0_TimerFrequencyGet,
+    .timerPeriodSet = (SYS_TIME_PLIB_PERIOD_SET)TC0_Timer16bitPeriodSet,
+    .timerCompareSet = (SYS_TIME_PLIB_COMPARE_SET)TC0_Timer16bitCompareSet,
+    .timerCounterGet = (SYS_TIME_PLIB_COUNTER_GET)TC0_Timer16bitCounterGet,
+};
+
+static const SYS_TIME_INIT sysTimeInitData =
+{
+    .timePlib = &sysTimePlibAPI,
+    .hwTimerIntNum = TC0_IRQn,
+};
+
+// </editor-fold>
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Local initialization functions
+// *****************************************************************************
+// *****************************************************************************
+
+/* MISRAC 2012 deviation block end */
+
+/*******************************************************************************
+  Function:
+    void SYS_Initialize ( void *data )
+
+  Summary:
+    Initializes the board, services, drivers, application and other modules.
+
+  Remarks:
+ */
+
+void SYS_Initialize ( void* data )
+{
+
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
+
+  
+    CLOCK_Initialize();
+    /* Configure Prefetch, Wait States */
+    PCHE_REGS->PCHE_CHECON = (PCHE_REGS->PCHE_CHECON & (~(PCHE_CHECON_PFMWS_Msk | PCHE_CHECON_ADRWS_Msk | PCHE_CHECON_PREFEN_Msk)))
+                                    | (PCHE_CHECON_PFMWS(1) | PCHE_CHECON_PREFEN(1) | PCHE_CHECON_ADRWS(0));
+
+
+
+	GPIO_Initialize();
+
+    SERCOM2_SPI_Initialize();
+
+    EVSYS_Initialize();
+
+	BSP_Initialize();
+    TC0_TimerInitialize();
+
+
+    /* MISRAC 2012 deviation block start */
+    /* Following MISRA-C rules deviated in this block  */
+    /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+    /* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+
+    /* Initialize SDSPI0 Driver Instance */
+    sysObj.drvSDSPI0 = DRV_SDSPI_Initialize(DRV_SDSPI_INDEX_0, (SYS_MODULE_INIT *)&drvSDSPI0InitData);
+
+
+    /* MISRA C-2012 Rule 11.3, 11.8 deviated below. Deviation record ID -  
+    H3_MISRAC_2012_R_11_3_DR_1 & H3_MISRAC_2012_R_11_8_DR_1*/
+        
+    sysObj.sysTime = SYS_TIME_Initialize(SYS_TIME_INDEX_0, (SYS_MODULE_INIT *)&sysTimeInitData);
+    
+    /* MISRAC 2012 deviation block end */
+
+    /*** File System Service Initialization Code ***/
+    (void) SYS_FS_Initialize( (const void *) sysFSInit );
+
+
+    /* MISRAC 2012 deviation block end */
+    APP_Initialize();
+
+
+    NVIC_Initialize();
+
+
+    /* MISRAC 2012 deviation block end */
+}
+
+/*******************************************************************************
+ End of File
+*/
